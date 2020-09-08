560a2daea688b312c10a176f0ba02e25614e2174
caidi <1289359053@qq.com>
2020-09-08 19:48:23 +0800

    x

--- leyou-item/leyou-item-service/src/main/java/com/leyou/controller/BrandController.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/controller/BrandController.java
@@ -1,21 +1,13 @@
 package com.leyou.controller;
 
-import com.leyou.common.enums.ResultCodeEnum;
 import com.leyou.common.page.PageResult;
-import com.leyou.common.result.Result;
 import com.leyou.pojo.Brand;
 import com.leyou.service.BrandService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
