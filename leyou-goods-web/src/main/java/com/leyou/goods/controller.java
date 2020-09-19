4351df3e0400a4e995d0ec6d4b663d431a40f1a2
caidi <caidif@163.com>
2020-09-19 15:10:55 +0800

    加入商品详情微服务

---
+++ leyou-goods-web/src/main/java/com/leyou/goods/controller.java
@@ -0,0 +1,10 @@
+package com.leyou.goods;
+
+/**
+ * @author: 蔡迪
+ * @date: 15:06 2020/9/19
+ * @description:
+ */
+public class controller {
+
+}
---
+++ leyou-goods-web/src/test/java/com/leyou/LeyouGoodsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeyouGoodsWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
