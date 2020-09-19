a4d2b748ee560c72a5abb5799df5d4ad461ebd71
caidi <caidif@163.com>
2020-09-19 15:01:11 +0800

    加入搜索微服务

--- leyou-item/leyou-item-service/src/main/java/com/leyou/controller/GoodsController.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/controller/GoodsController.java
@@ -8,7 +8,6 @@ import com.leyou.pojo.Spu;
 import com.leyou.pojo.SpuDetail;
 import com.leyou.request.SpuRequest;
 import com.leyou.service.GoodsService;
-import com.leyou.service.impl.GoodsServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
