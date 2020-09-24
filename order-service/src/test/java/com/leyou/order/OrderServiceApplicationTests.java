ca4ef6d4e9e8b94e4942f406fef764085436756c
caidi <caidif@163.com>
2020-09-24 11:28:23 +0800

    微服务补完

---
+++ order-service/src/test/java/com/leyou/order/OrderServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.leyou.order;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ user-service/src/test/java/com/leyou/user/UserServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.leyou.user;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
