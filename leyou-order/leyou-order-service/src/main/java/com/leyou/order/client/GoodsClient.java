4b1aa1ec3358d892e75ad0ef559d88caf0f1cead
caidi <1289359053@qq.com>
2020-10-06 17:55:16 +0800

    订单中心。支付也完善

---
+++ leyou-order/leyou-order-service/src/main/java/com/leyou/order/client/GoodsClient.java
@@ -0,0 +1,14 @@
+package com.leyou.order.client;
+
+
+import com.leyou.api.GoodsApi;
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * @Author: 98050
+ * Time: 2018-12-11 20:50
+ * Feature:商品FeignClient
+ */
+@FeignClient(value = "item-service")
+public interface GoodsClient extends GoodsApi {
+}
